12f38060f6c82db15fee3ce753bf07a06fe8ceb4
JANAPALLI Nataraju <nataraju.janapalli@hexagonsi.com>
2019-04-07 14:39:59 +0530

    Add project files.

---
+++ LoggedInUsers/Program.cs
@@ -0,0 +1,318 @@
+﻿using njanapal;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggedInUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isDisplayState = Convert.ToBoolean(ConfigurationManager.AppSettings["DisplayState"].ToString());
+            bool isDisplayIdleTime = Convert.ToBoolean(ConfigurationManager.AppSettings["DisplayIdleTime"].ToString());
+            bool isDisplayLogonTime = Convert.ToBoolean(ConfigurationManager.AppSettings["DisplayLogonTime"].ToString());
+            bool isDisplayID = Convert.ToBoolean(ConfigurationManager.AppSettings["DisplayID"].ToString());
+            bool isDisplaySessionName = Convert.ToBoolean(ConfigurationManager.AppSettings["DisplaySessionName"].ToString());
+
+            Console.WriteLine();
+            //Console.WriteLine($"Version : {Assembly.GetExecutingAssembly().GetName().Version}");
+            Console.WriteLine($"Product Version : {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion}");
+            Console.WriteLine("Note : Run 'AllowRemoteRPC.reg' registry file on the remote machine when remote machine state field is blank.");
+            string option = "Y";
+            Log log = new Log();
+
+            do
+            {
+                string fileFullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RemoteMachines.txt");
+
+                if (!File.Exists(fileFullPath))
+                {
+                    Console.WriteLine($" File doesnot exist at {fileFullPath} location.");
+                    return;
+                }
+
+                List<string> machines = new List<string>();
+                machines = File.ReadLines(fileFullPath).Where(r => !string.IsNullOrWhiteSpace(r) && !r.Trim().StartsWith("--")).Distinct().OrderBy(r => r).ToList();
+
+                if (machines == null || machines.Count == 0)
+                {
+                    Console.WriteLine($"No remote machine's configured in the {fileFullPath} file.");
+                    return;
+                }
+
+                RemoteMachine remote = new RemoteMachine(log);
+
+                //var items1 = remote.GetLoggedInUsersByMachine(machines);
+                //foreach (var item in items1)
+                //{
+                //    Console.WriteLine($"{item.RemoteMachine.ToUpper()} : ");
+                //    foreach (var user in item.Users)
+                //        Console.WriteLine($"\t{user}");
+                //}
+                //
+                //Console.WriteLine();
+                //Console.WriteLine();
+
+
+                //var items2 = remote.GetLoggedInUsersByUserName(machines);
+                //foreach (var item in items2)
+                //{
+                //    Console.WriteLine();
+                //    Console.WriteLine($"{item.User.ToUpper()} : ");
+                //
+                //    foreach (var remoteMachine in item.RemoteMachines)
+                //    {
+                //        Console.WriteLine($"\t{remoteMachine.RemoteMachine}");
+                //
+                //        Console.WriteLine($"\t\t{remoteMachine.}");
+                //        Console.WriteLine($"\t\t{remoteMachine}");
+                //
+                //    }
+                //    Console.WriteLine();
+                //    Console.WriteLine();
+                //}
+                //
+                //Console.WriteLine();
+                //Console.WriteLine();
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("\t1. Display details by User");
+                Console.WriteLine("\t2. Display details by Remote Mchine");
+                Console.WriteLine();
+                Console.Write("\tEnter your option : ");
+                var ch = Console.ReadLine();
+                ch = ch.Trim();
+
+                if (!ch.Equals("1") && !ch.Equals("2"))
+                    return;
+
+                Console.WriteLine();
+                log.Status = "Started";
+                var userDetails = remote.GetLoggedInUsers(machines);
+                log.Status = "Completed";
+
+                if (ch.Equals("1"))
+                {
+                    var users = userDetails.Select(r => r.UserName).Distinct().OrderBy(r => r).ToList();
+                    foreach (string user in users)
+                    {
+                        if (user == null || string.IsNullOrWhiteSpace(user))
+                            continue;
+
+                        Console.WriteLine();
+                        Console.WriteLine($"\t\t {user.ToUpper()} : ");
+
+                        var details = userDetails.Where(r => r.UserName.ToLower().Equals(user.ToLower())).OrderBy(r => r.RemoteMachine).ToList();
+                        if (details != null)
+                        {
+                            foreach (var d in details)
+                            {
+                                Console.WriteLine($"\t\t\t {d.RemoteMachine.ToUpper()}");
+
+                                if (isDisplayState)
+                                    Console.WriteLine($"\t\t\t\t State          : {d.State}");
+
+                                if (isDisplayIdleTime)
+                                    Console.WriteLine($"\t\t\t\t Idle Time      : {d.IdleTime}");
+
+                                if (isDisplayLogonTime)
+                                    Console.WriteLine($"\t\t\t\t Logon Time     : {d.LogonTime}");
+
+                                if (isDisplayID)
+                                    Console.WriteLine($"\t\t\t\t ID             : {d.Id}");
+
+                                if (isDisplaySessionName)
+                                    Console.WriteLine($"\t\t\t\t Session Name   : {d.SessionName}");
+
+                                //Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+                else if (ch.Equals("2"))
+                {
+                    var remoteMachines = userDetails.Select(r => r.RemoteMachine).Distinct().OrderBy(r => r).ToList();
+                    foreach (string remoteMachine in remoteMachines)
+                    {
+                        if (remoteMachine == null || string.IsNullOrWhiteSpace(remoteMachine))
+                            continue;
+
+                        Console.WriteLine();
+                        Console.WriteLine($"\t\t {remoteMachine.ToUpper()} : ");
+
+                        var details = userDetails.Where(r => r.RemoteMachine.ToLower().Equals(remoteMachine.ToLower())).OrderBy(r => r.UserName).ToList();
+                        if (details != null)
+                        {
+                            foreach (var d in details)
+                            {
+                                if (string.IsNullOrWhiteSpace(d.UserName) && string.IsNullOrWhiteSpace(d.State) && string.IsNullOrWhiteSpace(d.IdleTime) && string.IsNullOrWhiteSpace(d.LogonTime) && string.IsNullOrWhiteSpace(d.Id) && string.IsNullOrWhiteSpace(d.SessionName))
+                                    continue;
+
+                                Console.WriteLine($"\t\t\t {d.UserName.ToUpper()}");
+                                if (isDisplayState)
+                                    Console.WriteLine($"\t\t\t\t State          : {d.State}");
+
+                                if (isDisplayIdleTime)
+                                    Console.WriteLine($"\t\t\t\t Idle Time      : {d.IdleTime}");
+
+                                if (isDisplayLogonTime)
+                                    Console.WriteLine($"\t\t\t\t Logon Time     : {d.LogonTime}");
+
+                                if (isDisplayID)
+                                    Console.WriteLine($"\t\t\t\t ID             : {d.Id}");
+
+                                if (isDisplaySessionName)
+                                    Console.WriteLine($"\t\t\t\t Session Name   : {d.SessionName}");
+
+                                //Console.WriteLine();
+                            }
+                        }
+                    }
+
+
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.Write("Do you re-scan it again (Y/N)? : ");
+                option = Console.ReadLine();
+            } while (option.ToString().ToUpper().Equals("Y"));
+
+            Console.WriteLine();
+            //Console.ReadLine();
+        }
+
+        private static List<string> GetRemoteMachines()
+        {
+            string fileFullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RemoteMachines.txt");
+
+            return File.ReadLines(fileFullPath).Distinct().Where(r => !string.IsNullOrWhiteSpace(r)).ToList();
+        }
+
+        private static void Checkuser(string remoteMachineName)
+        {
+            if (string.IsNullOrWhiteSpace(remoteMachineName))
+            {
+                Console.WriteLine("usage: checkuser <machinename>");
+                return;
+            }
+
+            string strStatus = "There is no user logged into " + remoteMachineName;
+
+            Process[] ps = null;
+
+            try
+            {
+                ps = Process.GetProcesses(remoteMachineName);
+            }
+            catch
+            {
+                //GetProcesses will fail if the machine is off
+                strStatus = "Could not get process list from " + remoteMachineName;
+            }
+
+            if (ps != null)
+            {
+                foreach (Process p in ps)
+                {
+
+                    if (p.ProcessName.Equals("explorer"))
+                        strStatus = "There is a user logged into " + remoteMachineName;
+
+                }
+            }
+
+            Console.WriteLine(strStatus);
+
+        }
+
+
+        //public static string whoisLoggedIn(string strComputer)
+        //{
+        //
+        //    try
+        //    {
+        //
+        //
+        //
+        //        ConnectionOptions connection = new ConnectionOptions();
+        //
+        //
+        //        ManagementScope scope = new ManagementScope("\\\\" + strComputer + "\\root\\CIMV2", connection);
+        //        scope.Connect();
+        //
+        //
+        //
+        //        ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Process WHERE Name = 'explorer.exe'");
+        //
+        //
+        //        ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+        //
+        //
+        //        foreach (ManagementObject queryObj in searcher.Get())
+        //        {
+        //
+        //
+        //
+        //            ManagementPath path = new ManagementPath("Win32_Process.Handle='" + queryObj["Handle"] + "'");
+        //
+        //
+        //            ManagementObject classInstance = new ManagementObject(scope, path, null);
+        //
+        //
+        //            ManagementBaseObject outParams = classInstance.InvokeMethod("GetOwner", null, null);
+        //
+        //
+        //            return (outParams["User"].ToString());
+        //        }
+        //
+        //    }
+        //
+        //
+        //
+        //    catch (ManagementException err)
+        //    {
+        //
+        //
+        //
+        //        return ("NA");
+        //    }
+        //
+        //
+        //
+        //    catch (System.UnauthorizedAccessException unauthorizedErr)
+        //    {
+        //
+        //
+        //
+        //        return ("NA");
+        //    }
+        //
+        //
+        //
+        //    catch (Exception ex)
+        //    {
+        //
+        //
+        //
+        //        return ("NA");
+        //    }
+        //
+        //
+        //
+        //    return "NA";
+        //}
+
+    }
+}
---
+++ njanapal/RemoteMachine.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace njanapal
+{
+    public class RemoteMachine
+    {
+        //set HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\AllowRemoteRPC to 1
+
+        public Log Log { get; set; }
+        public RemoteMachine(Log log)
+        {
+            this.Log = log;
+        }
+        public List<RemoteMachineUser> LoggedUers { get; set; } = new List<RemoteMachineUser>();
+
+        public List<(string User, string Message, List<RemoteMachineUser> RemoteMachines)> GetLoggedInUsersByUserName(List<string> machines = null)
+        {
+            List<(string User, string Message, List<RemoteMachineUser> RemoteMachines)> result = new List<(string, string, List<RemoteMachineUser>)>();
+
+            List<RemoteMachineUser> info = this.LoggedUers;
+
+            if (machines != null) // && this.LoggedUers != null && this.LoggedUers.Count > 0)
+                info = GetLoggedInUsers(machines);
+
+            var users = info.Select(r => r.UserName).Distinct();
+
+            foreach (string user in users)
+            {
+                if (user == null || string.IsNullOrWhiteSpace(user))
+                    continue;
+
+                string message = string.Empty; //info.Where(r => r.User.Equals(user)).Select(r => r.Message).Distinct().FirstOrDefault().ToString();
+                List<RemoteMachineUser> nodenames = info.Where(r => r.UserName != null && r.UserName.Equals(user)).Select(r => r).Distinct().ToList();
+                result.Add((user, message, nodenames));
+            }
+
+            return result;
+        }
+
+        public List<(string RemoteMachine, string Message, List<string> Users)> GetLoggedInUsersByMachine(List<string> machines = null)
+        {
+            List<(string RemoteMachine, string Message, List<string> Users)> result = new List<(string, string, List<string>)>();
+
+            List<RemoteMachineUser> info = this.LoggedUers;
+
+            if (machines != null)
+                info = GetLoggedInUsers(machines);
+
+            var machinesTemp = info.Select(r => r.RemoteMachine).Distinct();
+
+            foreach (string machine in machinesTemp)
+            {
+                string message = info.Where(r => r.RemoteMachine.Equals(machine)).Select(r => r.Message).Distinct().FirstOrDefault().ToString();
+                List<string> users = info.Where(r => r.RemoteMachine.Equals(machine)).Select(r => r.UserName).Distinct().ToList();
+                result.Add((machine, message, users));
+            }
+
+            return result;
+        }
+
+        public List<RemoteMachineUser> GetLoggedInUsers(List<string> machines)
+        {
+            int i = 1;
+            foreach (string machineNodeName in machines)
+            {
+                if (string.IsNullOrWhiteSpace(machineNodeName))
+                    continue;
+
+                this.Log.Status = $"Processing Machine : {i++,3} / {machines.Count} - {machineNodeName}";
+
+                var (message, users, usersDetails) = GetLoggedInUsers(machineNodeName);
+
+                if (users == null || users.Count == 0)
+                    LoggedUers.Add(new RemoteMachineUser { RemoteMachine = machineNodeName, Message = message, UserName = string.Empty, SessionName = string.Empty, Id = string.Empty, State = string.Empty, IdleTime = string.Empty, LogonTime = string.Empty });
+                else
+                {
+                    foreach (string user in users)
+                    {
+                        var userDetails = usersDetails?.Where(r => r.UserName.ToLower().Equals(user.ToLower())).FirstOrDefault();
+                        if (userDetails == null)
+                            LoggedUers.Add(new RemoteMachineUser { RemoteMachine = machineNodeName, Message = message, UserName = user, SessionName = string.Empty, Id = string.Empty, State = string.Empty, IdleTime = string.Empty, LogonTime = string.Empty });
+                        else
+                        {
+                            LoggedUers.Add(new RemoteMachineUser { RemoteMachine = machineNodeName, Message = message, UserName = userDetails.UserName, SessionName = userDetails.SessionName, Id = userDetails.Id, State = userDetails.State, IdleTime = userDetails.IdleTime, LogonTime = userDetails.LogonTime });
+                        }
+                    }
+                }
+
+            }
+
+            return this.LoggedUers;
+        }
+
+        public (string, List<string>, List<QUser>) GetLoggedInUsers(string remoteComputerNodeName)
+        {
+            List<string> users = new List<string>();
+            List<QUser> usersDetails = new List<QUser>();
+            string message = string.Empty;
+
+            try
+            {
+                ConnectionOptions connection = new ConnectionOptions();
+                ManagementScope scope = new ManagementScope("\\\\" + remoteComputerNodeName + "\\root\\CIMV2", connection);
+                scope.Connect();
+
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Process WHERE Name = 'explorer.exe'");
+
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    ManagementPath path = new ManagementPath("Win32_Process.Handle='" + queryObj["Handle"] + "'");
+                    ManagementObject classInstance = new ManagementObject(scope, path, null);
+                    ManagementBaseObject outParams = classInstance.InvokeMethod("GetOwner", null, null);
+
+                    users.Add(outParams["User"].ToString());
+                }
+
+                usersDetails = GetQUserInfo(remoteComputerNodeName);
+
+            }
+            catch (ManagementException err)
+            {
+                message = $"ManagementException. Details: {err.ToString()}";
+            }
+            catch (System.UnauthorizedAccessException unauthorizedErr)
+            {
+                message = $"Doesn't have permissions to access the machine, Details : {unauthorizedErr.ToString()}";
+            }
+            catch (Exception ex)
+            {
+                message = $"Exception Details: {ex.ToString()}";
+            }
+
+            return (message, users, usersDetails);
+        }
+
+
+        public List<QUser> GetQUserInfo(string serverName)
+        {
+            string output = GetQUserInfo32(serverName);
+            if (string.IsNullOrWhiteSpace(output))
+                output = GetQUserInfo64(serverName);
+
+            if (string.IsNullOrWhiteSpace(output))
+                return null;
+
+            List<QUser> users = new List<QUser>();
+            string columnsString = output.Split(new string[] { "\r\n" }, StringSplitOptions.None).Where(r => r.Contains("USERNAME")).FirstOrDefault();
+            string[] lines = output.Split(new string[] { "\r\n" }, StringSplitOptions.None).Where(r => !r.Contains("USERNAME")).ToArray();
+
+            int i = 0;
+            foreach (string line in lines)
+            {
+                if (line.Contains("USERNAME") || string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                users.Add(new QUser(line, columnsString));
+            }
+
+            return users;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int Wow64EnableWow64FsRedirection(ref IntPtr ptr);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int Wow64RevertWow64FsRedirection(ref IntPtr ptr);
+
+        public string GetQUserInfo32(string serverName)
+        {
+            IntPtr val = IntPtr.Zero;
+            Wow64DisableWow64FsRedirection(ref val);
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = $"/c quser /SERVER:{serverName}";
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+
+            p.WaitForExit();
+
+            string output = p.StandardOutput.ReadToEnd();
+
+            return output;
+        }
+
+
+        public string GetQUserInfo64(string serverName)
+        {
+            IntPtr val = IntPtr.Zero;
+            Wow64RevertWow64FsRedirection(ref val);
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = $"/c quser /SERVER:{serverName}";
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+
+            List<QUser> users = new List<QUser>();
+            string output = p.StandardOutput.ReadToEnd();
+
+            return output;
+
+
+        }
+    }
+
+
+    public class RemoteMachineUser
+    {
+        public string RemoteMachine { get; set; }
+        public string Message { get; set; }
+        public string UserName { get; set; }
+        public string SessionName { get; set; }
+        public string Id { get; set; }
+        public string State { get; set; }
+        public string IdleTime { get; set; }
+        public string LogonTime { get; set; }
+    }
+
+
+    public class QUser
+    {
+        public string UserName { get; set; }
+        public string SessionName { get; set; }
+        public string Id { get; set; }
+        public string State { get; set; }
+        public string IdleTime { get; set; }
+        public string LogonTime { get; set; }
+
+        public QUser(string line, string columnsString)
+        {
+            UserName = line.Substring(columnsString.IndexOf("USERNAME"), columnsString.IndexOf("SESSIONNAME") - 1);
+            SessionName = line.Substring(columnsString.IndexOf("SESSIONNAME"), columnsString.IndexOf("ID") - columnsString.IndexOf("SESSIONNAME") - 1);
+            Id = line.Substring(columnsString.IndexOf("ID"), columnsString.IndexOf("STATE") - columnsString.IndexOf("ID") - 1);
+            State = line.Substring(columnsString.IndexOf("STATE"), columnsString.IndexOf("IDLE TIME") - columnsString.IndexOf("STATE") - 1);
+            IdleTime = line.Substring(columnsString.IndexOf("IDLE TIME"), columnsString.IndexOf("LOGON TIME") - columnsString.IndexOf("IDLE TIME") - 1);
+            LogonTime = line.Substring(columnsString.IndexOf("LOGON TIME"), line.Length - columnsString.IndexOf("LOGON TIME"));
+
+            UserName = UserName.Trim();
+            SessionName = SessionName.Trim();
+            Id = Id.Trim();
+            State = State.Trim().ToUpper().Equals("DISC") ? "Disconnected" : State.Trim();
+            IdleTime = IdleTime.Trim();
+            LogonTime = LogonTime.Trim();
+        }
+    }
+
+    public class Log
+    {
+        private StringBuilder LogMessages = new StringBuilder();
+
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                LogMessages.AppendLine(_status);
+                Console.WriteLine($"[{DateTime.Now }] {_status}");
+            }
+        }
+
+        //public string StatusInSameLine
+        //{
+        //    get { return _status; }
+        //    set
+        //    {
+        //        _status = value;
+        //        LogMessages.AppendLine(_status);
+        //        Console.WriteLine($"{DateTime.Now } {_status}");
+        //    }
+        //}
+
+        public string GetLogMessages()
+        {
+            return LogMessages.ToString();
+        }
+
+        public string WriteLog()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log");
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            path = Path.Combine(path, $"Log_{DateTime.Now.ToString("yyyyMMddHHmmssfff")}");
+
+            File.WriteAllText(path, LogMessages.ToString());
+
+            return path;
+        }
+
+    }
+
+}
